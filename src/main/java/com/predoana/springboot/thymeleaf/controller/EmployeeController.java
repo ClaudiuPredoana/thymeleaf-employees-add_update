8585c1984b56621b91008333af94fc7f3fe855fa
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-22 17:55:11 +0300

    Add controller code to pre-populate form

--- src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/predoana/springboot/thymeleaf/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.predoana.springboot.thymeleaf.entity.Employee;
 import com.predoana.springboot.thymeleaf.service.EmployeeService;
@@ -46,7 +43,21 @@ public class EmployeeController {
 		
 		return "employees/employee-form";
 	}
-	
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId,
+									Model theModel) {
+
+		//get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+
+		//set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
+
+		//send over to our form
+		return "/employees/employee-form";
+
+	}
 	@PostMapping("/save")
 	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
 		
