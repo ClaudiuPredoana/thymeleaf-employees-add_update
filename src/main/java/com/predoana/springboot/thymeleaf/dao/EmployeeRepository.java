f726846eb0aa32cdf72e83e1854d31bb504ea32f
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-22 17:37:29 +0300

    First commit

---
+++ src/main/java/com/predoana/springboot/thymeleaf/dao/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.predoana.springboot.thymeleaf.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.predoana.springboot.thymeleaf.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	//add method to sort by last name
+
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
+	
+	
+}
